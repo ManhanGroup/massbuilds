e6c9db9e6b8b8a03e37541b2cfe7a33d5e5850fe
Kaden Weber <kweber142@gmail.com>
2023-09-04 20:27:09 +0000

    Use fetch for development file uploads

--- ember/app/controllers/map/developments/uploads.js
+++ ember/app/controllers/map/developments/uploads.js
@@ -2,25 +2,23 @@ import Controller from '@ember/controller';
 import { service } from 'ember-decorators/service';
 import { action } from 'ember-decorators/object';
 import config from 'calbuilds/config/environment';
-
+import fetch from 'fetch';
 
 export default class extends Controller {
 
   @service map
   @service currentUser
   @service notifications
-  @service ajax
+
 
   @action
   uploadFile(file) {
     let uploadData = file.blob
     let formData = new FormData();
     formData.append('file', uploadData);
-    this.get('ajax')
-    .post(`${config.host}/developments/import`, {
-      contentType: false, // ??
-      processData: false,
-      data: formData, // ??
+    fetch(`${config.host}/developments/import`, {
+      method: 'POST',
+      body: formData,
     })
     .then(() => {
       this.get('notifications').show(
