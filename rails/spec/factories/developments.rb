85feb852ad64dfb429891d3bd900ebfd9834bd88
Kaden Weber <kweber142@gmail.com>
2023-10-17 15:43:53 -0400

    pass rspec examples

--- rails/spec/factories/developments.rb
+++ rails/spec/factories/developments.rb
@@ -11,22 +11,17 @@ FactoryBot.define do
     status { 'MyString' }
     descr { 'A sample development' }
     prj_url { 'http://www.example.com/' }
-    address { '123 Main Street' }
+    address { '101 Main Street' }
     state { 'CA' }
     zip_code { '95060' }
-    height { 1 }
-    stories { 1 }
     year_compl { 1 }
     prjarea { 1 }
     singfamhu { 1 }
-    smmultifam { 1 }
-    lgmultifam { 1 }
     hu { 1 }
     gqpop { 1 }
     rptdemp { 1 }
     commsf { 1 }
     hotelrms { 1 }
-    onsitepark { 1 }
     total_cost { 1 }
     ret_sqft { 1.5 }
     ofcmd_sqft { 1.5 }
@@ -53,15 +48,11 @@ FactoryBot.define do
     units_2bd { 1 }
     units_3bd { 1 }
     affrd_unit { false }
-    aff_u30 { 1 }
-    aff_30_50 { 1 }
     aff_50_80 { 1 }
-    aff_80p { 1 }
     headqtrs { false }
     park_type { 'parking' }
     publicsqft { 1 }
     yrcomp_est { '2010' }
-    mepa_id { 12345 }
     traffic_count_data { 'https://pems.dot.ca.gov' }
   end
 end
--- rails/spec/requests/edits_spec.rb
+++ rails/spec/requests/edits_spec.rb
@@ -97,8 +97,8 @@ RSpec.describe 'Edits', type: :request do
     end
 
     it 'fails on edits outside the covered geography for municipal users' do
-      development = FactoryBot.create(:development, municipal: 'Worcester', latitude: 42.2626, longitude: -71.8023, point: nil)
-      user = FactoryBot.create(:user, role: 'municipal')
+      development = FactoryBot.create(:development)
+      user = FactoryBot.create(:user, role: 'municipal', municipality: "Far Out")
       edit = FactoryBot.create(:edit, user: user, development: development)
       user_session = {
         Authorization: "Token token=#{user.authentication_token}, email=#{user.email}",
