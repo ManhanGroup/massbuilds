fb1ec5d6e62ac1d62d7df6556f1f7efb192a4cdf
Kaden Weber <kweber142@gmail.com>
2023-10-18 23:36:01 -0400

    remove unused references to google analytics

--- ember/app/router.js
+++ ember/app/router.js
@@ -4,17 +4,6 @@ import config from './config/environment';
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
-  didTransition: function() {
-    this._super(...arguments);
-
-    // ga() undefined but should be from google analytics, should be able to add an import here
-    // commented out as it is likely not working. see issue #2
-    // there are ga(...) calls in ember/app/index.html
-    // return ga('send', 'pageview', {
-    //   'page': this.get('url'),
-    //   'title': this.get('url')
-    // });
-  }
 });
 
 Router.map(function() {
